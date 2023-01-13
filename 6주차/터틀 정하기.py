b55b4e0711add45d80767c96e02691046f0e855c
ksj943 <120165389+ksj943@users.noreply.github.com>
2023-01-13 17:40:58 +0900

    Add files via upload

---
+++ 6주차/터틀 정하기.py
@@ -0,0 +1,10 @@
+import turtle
+t = turtle.Turtle(shape = "turtle")
+s = "즐거운 씨큐브 코딩"
+n = turtle.numinput(s, "앞으로 얼마나 이동할까요?")
+t.forward(n)
+ang = turtle.numinput(s, "오른쪽으로 얼마나 회전할까요?:", default = 0, minval = 0,maxval = 360)
+t.right(ang)
+n = turtle.numinput(s, "앞으로 얼마나 이동할까요?")
+t.forward(n)
+turtle.done()
